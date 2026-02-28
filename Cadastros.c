77138c9f281f0476db84d12983729ae606636211
MXC <maxwellbom93@gmail.com>
2026-02-28 16:57:04 +0000

    SALVANDO MINHA APRENDIZAGEM AQUI

---
+++ Cadastros.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+
+    printf("Digite seu nome: \n");
+    scanf("%s", &nome);
+
+    printf("Digite sua matricula: \n");
+    scanf("%d", &matricula);
+
+    printf("Nome do aluno: %s - Matricula: %d\n", nome, matricula);
+    printf("Idade: %d - altura: %.2f", idade, altura);
+
+    return 0;
+
+}
---
+++ dados01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    int idade = 25;
+    float altura = 1.75;
+    char opcao = 'S';
+    char nome[20] = "sergio";
+
+    printf("A idade do %s é: %d\n", nome, idade);
+    printf("A altura é: %f\n", altura);
+    printf("A opção é: %c\n", opcao);
+}
